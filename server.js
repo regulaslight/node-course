3e4488b7119490a72c2c4385949323394d5aae46
Rahul Ramesh <rahulramesh118@gmail.com>
2018-01-07 12:18:00 +0530

    Added start script to package.json and modiefied port variable in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -64,7 +65,10 @@ app.get('/bad' , (req,res) => {
     });
 
 });
-app.listen(3000);
+app.listen(port , ()=>{
+
+  console.log(`App running on port ${port}`);
+});
 
 // new route bad
 // send json with error message property
